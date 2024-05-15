602399746e8f9f519ddae320c4567e462932b0c0
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-05-15 23:35:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//assignment # 4 //
+var authorname = "Imam ALI(a.s)";
+var famousquote = "This entire world is not worth a single tear";
+console.log("".concat(authorname, " once said '").concat(famousquote, "'"));
+//assihnment # 5 //
+var famous_person = "Imam Ali(a.s)";
+var meessage = "This entire world is not worth a single tear";
+console.log("".concat(authorname, " once said '").concat(meessage, "'"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+//assignment # 4 //
+let authorname = "Imam ALI(a.s)"
+let famousquote="This entire world is not worth a single tear"
+console.log(`${authorname} once said '${famousquote}'`)
+
+//assihnment # 5 //
+let famous_person = "Imam Ali(a.s)"
+let meessage = "This entire world is not worth a single tear"
+console.log(`${authorname} once said '${meessage}'`)
